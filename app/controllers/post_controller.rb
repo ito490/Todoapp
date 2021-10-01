8a8d476d6b569d7105c548ccd2bfd871c4d2b38b
ito490 <ito.ss490@gmail.com>
2021-10-01 22:01:02 +0900

    編集機能追加

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -8,5 +8,24 @@ class PostController < ApplicationController
   end
 
   def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    
+    @post.save
+    redirect_to("/post/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    
+    redirect_to("/post/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'post/index'
+  get "post/index"
   get "post/new" => "post#new"
   get "post/:id" => "post#show"
-  get 'home/top'
+  post "post/create" => "post#create"
+  get "post/:id/edit" => "post#edit"
+  post "post/:id/update" => "post#update"
+  get "home/top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'home/about'
+  get "home/about"
 end
