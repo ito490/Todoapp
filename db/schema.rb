3c4443383ad734d771d2d19d56229ee955ba955a
ito490 <ito.ss490@gmail.com>
2021-10-27 13:19:24 +0900

    パスワードをハッシュ化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_032426) do
+ActiveRecord::Schema.define(version: 2021_10_27_040419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_10_22_032426) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
